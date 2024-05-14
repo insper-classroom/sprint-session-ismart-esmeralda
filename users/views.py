31d5b378aba6d3dc722335e0f4412c26f7234290
Gustavo Valente <159027272+gustavovvbs@users.noreply.github.com>
2024-05-14 13:28:01 -0300

    ta dando pra logar e registrar usuarios

--- users/views.py
+++ users/views.py
@@ -1,40 +1,52 @@
+from django.shortcuts import render
+
+# Create your views here.
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from .forms import CustomUserCreationForm
-from django.http import HttpResponseRedirect
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
+from .forms import LoginForm, RegisterForm
+from django.http import HttpResponse
 
-
-def signupview(request):
-    """View para registro de usuário"""
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            #aqui eh pra onde vai redirecionar o usuário depois de registrar
-            return redirect('login')
-    else:
-        form = CustomUserCreationForm()
-    return render(request, 'users/signup.html', {'form': form})
-
-def loginview(request):
-    """View para login de usuário"""
+def sign_up(request):
+    print(request)
     if request.method == 'GET':
-        return render(request, 'users/login.html')
+        form = RegisterForm()
+        return render(request, 'register.html', { 'form': form})   
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, 'Voce logou, deu certo amigo parabems')
+            login(request, user)
+            return HttpResponse('BOAAAAAAAAA')
+        else:
+            return render(request, 'register.html', {'form': form})
+
+def sign_in(request):
+    if request.method == 'GET':
+        form = LoginForm()
+        return render(request, 'login.html', {'form': form})
 
     elif request.method == 'POST':
-        email = request.POST.get('email', '')
-        password = request.POST.get('password', '')
+        form = LoginForm(request.POST)
+        print(form.is_valid())
 
-        user = authenticate(request, username = email, password = password)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
 
-        if user is not None:
-            login(request, user)
-            if user.is_staff:
-                #se o usuario for colaborador, redireciona pra ca 
-                return HttpResponseRedirect('/admin')
-            else:
-                #caso contrario ele é aluno, e redireciona pra ca
+            user = authenticate(request, username = username, password = password)
+            print(user)
+            if user:
+                login(request, user)
+                messages.success(request, f'Logado com sucesso')
                 return redirect('index')
-        else:
-            return HttpResponse('deu ruim pae')
+
+        messages.error(request, f'usuario ou senhas invalidos')
+        return render(request,'login.html',{'form': form})
+
+
+
