070e20a652555566c0562286e3526cf65be5c8fb
Gustavo Valente <159027272+gustavovvbs@users.noreply.github.com>
2024-05-14 13:34:54 -0300

    comentarios e gitignore

--- users/views.py
+++ users/views.py
@@ -8,43 +8,47 @@ from .forms import LoginForm, RegisterForm
 from django.http import HttpResponse
 
 def sign_up(request):
-    print(request)
+    #
     if request.method == 'GET':
         form = RegisterForm()
         return render(request, 'register.html', { 'form': form})   
 
+    #se for um post ele vai validar o formulario e registrar o usuario no BD 
     if request.method == 'POST':
         form = RegisterForm(request.POST)
+        #se o formulario for valido ele salva o usuario no, loga, e volta pro index
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
             user.save()
-            messages.success(request, 'Voce logou, deu certo amigo parabems')
+            messages.success(request, 'Usuário registrado com sucesso!')
             login(request, user)
-            return HttpResponse('BOAAAAAAAAA')
+            return redirect('index')
+        #se o formulario for invalido ele volta pro formulario
         else:
             return render(request, 'register.html', {'form': form})
 
 def sign_in(request):
+    #se for um get renderiza o formulario
     if request.method == 'GET':
         form = LoginForm()
         return render(request, 'login.html', {'form': form})
 
+    #se for um post ele vai validar o formulario
     elif request.method == 'POST':
         form = LoginForm(request.POST)
-        print(form.is_valid())
-
+        
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
 
             user = authenticate(request, username = username, password = password)
-            print(user)
+            #se o login der certo, faz isso
             if user:
                 login(request, user)
                 messages.success(request, f'Logado com sucesso')
                 return redirect('index')
-
+        #se o login der errado, faz isso
         messages.error(request, f'usuario ou senhas invalidos')
         return render(request,'login.html',{'form': form})
 
