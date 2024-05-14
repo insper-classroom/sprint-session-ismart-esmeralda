845dd87789cf0888e1c193cb83e0ad64deceffc4
BRUNOOBERHUBER <bruno.oberhuber@gmail.com>
2024-05-14 17:15:17 -0300

    merge

--- atendimento/urls.py
+++ atendimento/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('aluno', views.aluno, name='aluno'),
     path('', views.index, name='index'),
+    path('duvidas/', views.duvidas, name='duvidas')
+    
 ]
--- users/forms.py
+++ users/forms.py
@@ -1,18 +1,14 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
-from .models import CustomUser
 
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=65)
+    password = forms.CharField(max_length=65, widget=forms.PasswordInput)
+    
 
-class CustomUserCreationForm(UserCreationForm):
-
+class RegisterForm(UserCreationForm):
     class Meta:
-        model = CustomUser
-        fields = ("email", "name", "concurso")
-
-
-
-class CustomUserChangeForm(UserChangeForm):
-
-    class Meta:
-        model = CustomUser
-        fields = ("email", "name", "concurso")
+        model=User
+        fields = ['username','email','password1','password2'] 
