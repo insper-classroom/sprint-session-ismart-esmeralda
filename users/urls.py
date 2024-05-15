820ef9e1d7cd1173bb4485ffc15eed05af7d0c41
Gustavo Valente <159027272+gustavovvbs@users.noreply.github.com>
2024-05-15 09:57:27 -0300

    cada tela de recuperacao de senha tem um html separado, recuperacao de senha ta funcionando, e separei os htmls de autenticacao em paginas

--- users/urls.py
+++ users/urls.py
@@ -7,11 +7,11 @@ urlpatterns = [
     path('login/', views.sign_in, name='login'),
     path('logout/', views.sign_out, name='logout'),
 
-    path('reset_password', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    path('reset_password', auth_views.PasswordResetView.as_view(template_name = 'autenticacao/password_reset.html'), name='reset_password'),
 
-    path('reset_password_sent', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset_password_sent', auth_views.PasswordResetDoneView.as_view(template_name = 'autenticacao/password_reset_sent.html'), name='password_reset_done'),
 
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name = 'autenticacao/password_reset_form.html'), name='password_reset_confirm'),
 
-    path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('reset_password_complete', auth_views.PasswordResetCompleteView.as_view(template_name = 'autenticacao/password_reset_complete.html'), name='password_reset_complete'),
 ]
