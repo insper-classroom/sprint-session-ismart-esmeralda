76a346740ea8d1bc950ca3b1b1dc3019c495e2b7
Gustavo Valente <159027272+gustavovvbs@users.noreply.github.com>
2024-05-16 18:40:53 -0300

    consegui botar um modelo generic de foreign key (agora, sempre q eu referenciar um usuario ou um colab qnd criar uma mensagem, necessariamente o usuario e o colaborador vao ser os dois que sao daquela conversa

--- atendimento/views.py
+++ atendimento/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from twilio.rest import Client
+from django.http import HttpResponse
+from .models import Colaborador, Conversa, Usuario, Mensagem
+from django.contrib.contenttypes.models import ContentType
+
+
 
 # Create your views here.
 def index(request):
@@ -12,3 +18,37 @@ def duvidas(request):
 @login_required
 def aluno(request):
     return render(request, 'atendimento/aluno.html')
+
+
+def testchat(request):
+    account_sid = 'AC4001f4f9199704babdc1297dfffeabda'
+    auth_token = '7f9724a8f537cec4e85ac1d86c50b660'
+    client = Client(account_sid, auth_token)
+    messages = client.messages.list()
+    messages.reverse()
+    return render(request, 'atendimento/testchat.html', {'messages': messages})
+
+def testeregistro(request):
+    user = Usuario.objects.create(nome='Alice')
+    collaborator = Colaborador.objects.create(nome='Bob')
+    return HttpResponse('Usuário e colaborador criados com sucesso!')
+
+def colaborador_conversas(request, colaborador_id):
+    user = Usuario.objects.get(pk=1)
+    collaborator = Colaborador.objects.get(pk=colaborador_id)
+    conversa = Conversa.objects.create(usuarios=user, colaboradores=collaborator)
+    Mensagem.objects.create(conversa=conversa, sender=user, content='Hello, Bob!')
+    Mensagem.objects.create(conversa=conversa, sender=collaborator, content='Hi, Alice!')
+    conversa2 = Conversa.objects.create(usuarios = user, colaboradores = collaborator)
+    Mensagem.objects.create(conversa = conversa2, sender = user, content = 'hell imakmfr')
+    Mensagem.objects.create(conversa = conversa2, sender = collaborator, content = 'hi imakmfr')
+
+    for mensagem in conversa.mensagens.all():
+        print(mensagem.content)
+
+    
+    colaborador = Colaborador.objects.get(pk = colaborador_id)
+    conversas = Conversa.objects.filter(colaboradores = colaborador)
+    return render(request, 'atendimento/testconversas.html', {'colaborador': colaborador, 'conversas': conversas})
+
+
