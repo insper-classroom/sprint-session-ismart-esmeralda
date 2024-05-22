3d6f062ae5fb1a6c81ffa5976853a63bc3fb9fbd
Gustavo Valente <159027272+gustavovvbs@users.noreply.github.com>
2024-05-22 13:49:20 -0300

    tem as estatistica e agr ta mais bonitinho

--- atendimento/urls.py
+++ atendimento/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('receberzap/', views.receber_zap, name='receber_zap'),
     path('tela_colaborador/resolve/<int:conversa_id>/', views.resolve, name='resolve'),
     path('tela_colaborador/assign_conversa/<int:conversa_id>/', views.assign_conversa, name='assign_conversa'),
+    path('tela_colaborador/sendmsg/<int:telefone>/<int:conversa_id>/', views.send_msg, name='send_msg'),
 
     
 
