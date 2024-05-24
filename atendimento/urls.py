439c7ffdd4e8638836dba5140818e1b6fb1afb83
jota <joaopedroamiguel@gmail.com>
2024-05-24 10:51:21 -0300

    Bug fix

--- atendimento/urls.py
+++ atendimento/urls.py
@@ -13,8 +13,7 @@ urlpatterns = [
     path('tela_colaborador/assign_conversa/<int:conversa_id>/', views.assign_conversa, name='assign_conversa'),
     path('tela_colaborador/sendmsg/<int:telefone>/<int:conversa_id>/', views.send_msg, name='send_msg'),
     path('enviar_email/', views.mandar_email, name='mandar_email'),
-    path('receber_emails/', views.receive_email, name='receive_email')
-
+    path('receber_emails/', views.receive_email, name='receive_email'),
     path('duvidas/', views.duvidas, name='duvidas'),
     path('chat_nao_atribuido/', views.chat_nao_atribuido, name='chat_nao_atribuido')
     
