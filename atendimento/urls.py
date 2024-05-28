59322cf5d5d0a8dfa7b60f8103ee34d14648a1b6
jota <joaopedroamiguel@gmail.com>
2024-05-28 17:02:44 -0300

    Melhorando o fluxo de telas do site

--- atendimento/urls.py
+++ atendimento/urls.py
@@ -38,13 +38,6 @@ urlpatterns = [
     path('side_minhas_conversas/mandar_email/<str:user_email>/<int:conversa_id>/', views.mandar_email, name='mandar_email'),
     
     #view pra ficar periodicamente checando novos emails
-    path('receive_email/', views.receive_email, name='receive_email'),
-    path('tela_colaborador/resolve/<int:conversa_id>/', views.resolve, name='resolve'),
-    path('tela_colaborador/assign_conversa/<int:conversa_id>/', views.assign_conversa, name='assign_conversa'),
-    path('tela_colaborador/sendmsg/<int:telefone>/<int:conversa_id>/', views.send_msg, name='send_msg'),
-    path('enviar_email/', views.mandar_email, name='mandar_email'),
-    path('receber_emails/', views.receive_email, name='receive_email'),
-    path('duvidas/', views.duvidas, name='duvidas'),
-    path('chat_nao_atribuido/', views.chat_nao_atribuido, name='chat_nao_atribuido')
+    path('receive_email/', views.receive_email, name='receive_email')
     
 ]
